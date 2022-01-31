2142efbb3e1e844f25c0fc2c6100c1c74ede3f2c
denissonsillva <denisson.sillva@gmail.com>
2022-01-31 20:54:00 -0300

    Projetos App Android Kotlin CARROS - capítulo 16 (16.4 - Parser de JSON com a lib Google Gson)

--- app/src/main/java/br/com/livroandroid/carros/domain/CarroService.kt
+++ app/src/main/java/br/com/livroandroid/carros/domain/CarroService.kt
@@ -1,14 +1,14 @@
 package br.com.livroandroid.carros.domain
 
 import android.content.Context
-import android.util.Log
 import br.com.livroandroid.carros.R
-import org.json.JSONArray
+import br.com.livroandroid.carros.extensions.fromJson
 
 object CarroService {
     private const val TAG = "livro"
+
     //Busca os carros por tipo
-    fun getCarros(context: Context, tipo:TipoCarro): List<Carro> {
+    fun getCarros(context: Context, tipo: TipoCarro): List<Carro> {
         //Este é o arquivo que deve ser lido
         val raw = getArquivoRaw(tipo)
         //Abre o arquivo para leitura
@@ -17,38 +17,20 @@ object CarroService {
         inputStream.bufferedReader().use {
             //Lê o JSON e cria a lista de carros
             val json = it.readText()
-            val carros = parserJson(json)
+            //Convert o JSON para List<Carro>
+            val carros = fromJson<List<Carro>>(json)
             return carros
         }
     }
+
     //Retorna o arquivo que deve ser lido para o tipo informado
-    private fun getArquivoRaw(tipo: TipoCarro) = when(tipo) {
+    private fun getArquivoRaw(tipo: TipoCarro) = when (tipo) {
         TipoCarro.Classicos -> R.raw.carros_classicos
         TipoCarro.Esportivos -> R.raw.carros_esportivos
         else -> R.raw.carros_luxo
     }
-
-    //Lê o json e cria a lista de carro
-    private  fun parserJson(json: String): List<Carro> {
-        val carros = mutableListOf<Carro>()
-        //Cria um array com este JSON
-        val array = JSONArray(json)
-        //Percorre cada carro (JSON)
-        for(i in 0 until array.length()) {
-            //jason do carro
-            val jsonCarro = array.getJSONObject(i)
-            val c = Carro()
-            //Lê as informações de cada carro
-            c.nome = jsonCarro.optString("nome")
-            c.desc = jsonCarro.optString("desc")
-            c.urlFoto = jsonCarro.optString("url_foto")
-            carros.add(c)
-        }
-
-    Log.d(TAG, "${carros.size} carros encontrados.")
-    return carros
-    }
 }
+
 
 
 
